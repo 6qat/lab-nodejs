9e2c6d1b0990521f3ed1a07e471405d1f6b4f00c
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2023-09-27 09:35:31 -0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import {add, concat, multiply} from './operations';
+
+let i: number = 31;
+
+console.log(add(i, 3));
+
+let j = multiply(i, 3, 2);
+
+console.log(j);
+
+let s = concat('Hello', ' world');
+
+console.log(s);
---
+++ src/operations.ts
@@ -0,0 +1,11 @@
+export function add(a: number, b: number): number {
+    return a + b;
+}
+
+export function multiply(a: number, b: number, c: number): number {
+    return a * b * c;
+}
+
+export function concat(a: string, b: string): string {
+    return a + b;
+}
