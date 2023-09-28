c73e0f85c4aab2a60c4ffd7953df2ba4d3045616
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2023-09-28 16:42:14 -0300

    Javascript really really really sucks.

--- src/methods.js
+++ src/methods.js
@@ -21,3 +21,18 @@ let shift = a.shift();
 console.log("a (after shift): ", a);
 console.log("shift: ", shift);
 
+let a2 = ['a', 'b', 'c'];
+console.log(a2.slice(0, 1));
+console.log(a2.slice(1));
+console.log(a2.slice(1, 2));
+console.log(a2.slice(2, 1));
+
+let n = [4, 8, 15, 16, 23, 42];
+n.sort();
+console.log(n);   // this really really really sucks!!!!!!!
+
+n.sort(function (a, b) {
+    return a - b;
+});
+console.log(n);
+
